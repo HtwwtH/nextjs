cd61a95b19c3a9e988e649d9102d105061629f55
Alina Povazhnyuk <alina.l22@mail.ru>
2021-08-09 15:22:44 +0700

    rating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Button, P, Tag, Title } from '../components';
+import React, { useEffect, useState } from 'react';
+import { Button, P, Rating, Tag, Title } from '../components';
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Title tag='h1'>H1 title</Title>
       <div className="indent"></div>
-      <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <Button appearance='primary' arrow='right' onClick={() => setCounter(counter + 1)}>Кнопка</Button>
       <Button appearance='ghost' arrow='right'>Кнопка</Button>
       <div className="indent"></div>
       <P>В данном курсе по выращиванию суккулентов вы получите необходимые знания и практические навыки по выращиванию и уходу за суккулентами. После прохождения курса вы сможете вырастить симпатичное, редкое и неприхотливое растение. </P>
@@ -20,6 +22,8 @@ export default function Home(): JSX.Element {
       <Tag size='s' color='green'>s green</Tag>
       <Tag size='s' color='red'>s red</Tag>
       <Tag size='s' color='primary'>s primary</Tag>
+      <div className="indent"></div>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
